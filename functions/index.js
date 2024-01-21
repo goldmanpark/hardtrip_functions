fcfee7b74b5a2b1c8ff18d49351c14b02da1ede8
Goldman Park <death2501@gmail.com>
2024-01-21 20:43:24 +0900

    test

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,29 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const { logger } = require("firebase-functions");
+const { onRequest, onCall } = require("firebase-functions/v2/https");
+const { initializeApp } = require("firebase-admin/app");
+const axios = require('axios');
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+exports.get_trip_advisor_info = onRequest({ cors: true }, async (req, res) => {
+  try {
+    const apiResponse = await axios.get('https://api.content.tripadvisor.com/api/v1/location/search', {
+      params: {
+        key: process.env.TRIPADVISOR_API_KEY,
+        searchQuery: req.query.searchQuery,
+        latLong: `${req.query.lat},${req.query.lng}`,
+        language: 'en',
+      },
+    });
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    res.set('Access-Control-Allow-Origin', '*');
+    res.json(apiResponse.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+exports.test = onCall({ cors: true }, (data) => {
+  return 'fuck'
+})
